0c086459d02218e72160a8123f936c3850293e79
hamalainenmiska100-hue <hamalainen.miska100@gmail.com>
2026-03-27 15:31:06 +0200

    Update ContentView.swift

--- Codex/ContentView.swift
+++ Codex/ContentView.swift
@@ -2,11 +2,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
-
-            CodexWebView(url: URL(string: "https://chatgpt.com/codex")!)
-                .ignoresSafeArea()
-        }
+        CodexWebView(url: URL(string: "https://chatgpt.com/codex")!)
+            .ignoresSafeArea()
+            .background(Color.black)
     }
 }
